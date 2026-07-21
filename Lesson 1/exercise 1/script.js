d2f78e8a9b94a52576b033078c7183bbf7e2cfca
Aqheel <aqheelsharief@gmail.com>
2026-07-21 11:17:44 +0530

    Learning JS

---
+++ Lesson 1/exercise 1/script.js
@@ -0,0 +1,3 @@
+console.log("I am learning Javascript.");
+console.log("My favourite language is Python.");
+console.log("Soon I will build awesome websites");
---
+++ lesson 2/script.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 20;
+
+console.log(a);
+
+a = b;
+
+console.log(a);
+
+b = 50;
+
+console.log(a);
+
+console.log(b);
