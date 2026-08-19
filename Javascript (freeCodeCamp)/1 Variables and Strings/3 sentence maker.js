574960369f008a84134105e5e2c3b1dd1782977c
Aqheel <aqheelsharief@gmail.com>
2026-08-19 13:25:30 +0530

    practice projects

---
+++ Javascript (freeCodeCamp)/1 Variables and Strings/3 sentence maker.js
@@ -0,0 +1,16 @@
+let adjective = "beautiful";
+let noun = "woman";
+let verb = "checked";
+let place = "Tolichowki";
+let adjective2 = "small";
+let noun2 = "Biryani";
+let firstStory = "Once upon a time, there was a(n) " + adjective +" "+ noun + " who loved to eat " + noun2 + ". The " + noun +" lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+adjective = "handsome";
+noun = "man";
+verb = "blown";
+place = "Mehdipatnam";
+adjective2 = "big";
+noun2 = "tahari";
+let secondStory = "Once upon a time, there was a(n) " + adjective +" "+ noun + " who loved to eat " + noun2 + ". The " + noun +" lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory)
