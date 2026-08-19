5b0b6612ff177baaf84c8321f0cc853a513dfa8d
Aqheel <aqheelsharief@gmail.com>
2026-08-19 12:38:29 +0530

    practice projects

---
+++ Javascript/1 Variables and Strings/2 trivia bot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Aqqi";
+let botLocation = "India";
+let favoriteLanguage = "Python";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " +  favoriteLanguage + ".");
+let codingFact = favoriteLanguage + " is the easiest.";
+console.log(codingFact)
+codingFact = favoriteLanguage + " is a high level language";
+console.log(codingFact)
+codingFact = favoriteLanguage + " is so close to simple english";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
