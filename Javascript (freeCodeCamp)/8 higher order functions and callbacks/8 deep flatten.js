79244f5789e30c8cd86a62c9928788a50d92de41
Aqheel <aqheelsharief@gmail.com>
2026-08-31 01:24:50 +0530

    completed a lab

---
+++ Javascript (freeCodeCamp)/8 higher order functions and callbacks/8 deep flatten.js
@@ -0,0 +1,13 @@
+function steamrollArray(arr) {
+  const result = [];
+
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      result.push(...steamrollArray(item));
+    } else {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
