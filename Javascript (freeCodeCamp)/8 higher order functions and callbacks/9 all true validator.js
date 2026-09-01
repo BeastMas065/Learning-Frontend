d212fbfe7de83076ebe0fa20beebce13ef9bd739
Aqheel <aqheelsharief@gmail.com>
2026-09-01 23:23:35 +0530

    update

--- Javascript (freeCodeCamp)/8 higher order functions and callbacks/9 all true validator.js
+++ Javascript (freeCodeCamp)/8 higher order functions and callbacks/9 all true validator.js
@@ -1,5 +1,3 @@
-function truthCheck(arr, str) {
-  return arr.every(item => item[str]);
-}
+const truthCheck = (arr, str) => arr.every(item => item[str]);
 
 truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
